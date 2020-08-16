12c2a183089d1605dd22ea3752facea01a00edee
vtittert <vtittert@ucsc.edu>
2020-08-15 21:00:48 -0700

    make numbers on board correct

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -4,7 +4,7 @@ var ctx = c.getContext("2d");
 class Board {
 
 	constructor(squaresX, squaresY, leftX, topY, width, height) {
-		this.numColors = ["#000000", "#0000FF", "#20B000", "#FF0000"];
+		this.numColors = ["#000000", "#0000FF", "#20B000", "#FF0000", "#B000B0", "#B01010", "#40E0D0", "#000000", "#606060"];
 		this.squaresX = squaresX;
 		this.squaresY = squaresY;
 		this.leftX = leftX;
@@ -15,20 +15,63 @@ class Board {
 		this.squareHeight = height / squaresY;
 		console.log("square width: " + this.squareWidth);
 		console.log("square height: " + this.squareHeight);
-		this.placeBombs();
-		this.draw();
+		this.PlaceBombs(1);
+		this.CreateNums();
+		this.MakeCover();
+		this.Draw();
 	}
-	placeBombs() {
+	PlaceBombs(diff) { // diff should range from 1 to 5
 		this.squareValue = [];
+		var numOfMines = 0;
 		for (var x = 0; x < this.squaresX; x++) {
 			var tempCol = [];
 			for (var y = 0; y < this.squaresY; y++) {
-				tempCol.push(Math.floor(Math.random() * 5 - 1));
+				if (Math.random() * (25 + diff) > 23) {
+					tempCol.push(-1);
+					numOfMines++;
+				}
+				else
+					tempCol.push(0);
 			}
 			this.squareValue.push(tempCol);
 		}
+		console.log(numOfMines + " mines placed.");
 	}
-	draw() {
+	CreateNums() {
+		for (var x = 0; x < this.squaresX; x++) {
+			for (var y = 0; y < this.squaresY; y++) {
+				if (this.squareValue[x][y] != -1) {
+					var tempNum = 0;
+					tempNum += this.SquareVal(x - 1, y - 1);
+					tempNum += this.SquareVal(x, y - 1);
+					tempNum += this.SquareVal(x + 1, y - 1);
+					tempNum += this.SquareVal(x - 1, y);
+					tempNum += this.SquareVal(x + 1, y);
+					tempNum += this.SquareVal(x - 1, y + 1);
+					tempNum += this.SquareVal(x, y + 1);
+					tempNum += this.SquareVal(x + 1, y + 1);
+					this.squareValue[x][y] = tempNum;
+				}
+			}
+		}
+	}
+	SquareVal(x, y) { // returns 1 if the square is a mine, 0 otherwise
+		if (x >= 0 && y >= 0 && x < this.squaresX && y < this.squaresY && this.squareValue[x][y] == -1) {
+			return 1;
+		}
+		return 0
+	}
+	MakeCover() {
+		this.covered = [];
+		for (var x = 0; x < this.squaresX; x++) {
+			var tempCol = [];
+			for (var y = 0; y < this.squaresY; y++) {
+				tempCol.push(true);
+			}
+			this.covered.push(tempCol);
+		}
+	}
+	Draw() {
 		ctx.fillStyle = "#C8C8C8";
 		ctx.fillRect(this.leftX, this.topY, this.width, this.height);
 
@@ -46,37 +89,61 @@ class Board {
 		ctx.lineWidth = 1;
 		ctx.beginPath();
 		for (var y = 1; y < this.squaresY; y++) {
-			ctx.moveTo(this.leftX, this.topY + this.squareHeight * y + 0.5);
-        	ctx.lineTo(this.leftX + this.width, this.topY + this.squareHeight * y + 0.5);
+			ctx.moveTo(this.leftX, this.topY + this.squareHeight * y - 0.5);
+        	ctx.lineTo(this.leftX + this.width, this.topY + this.squareHeight * y - 0.5);
 		}
 		for (var x = 1; x < this.squaresX; x++) {
-			ctx.moveTo(this.leftX + this.squareWidth * x + 0.5, this.topY);
-        	ctx.lineTo(this.leftX + this.squareWidth * x + 0.5, this.topY + this.height);
+			ctx.moveTo(this.leftX + this.squareWidth * x - 0.5, this.topY);
+        	ctx.lineTo(this.leftX + this.squareWidth * x - 0.5, this.topY + this.height);
 		}
 		ctx.stroke();
 
-		ctx.font = "30px Arial"; // numbers
+		ctx.font = "20px Arial"; // squares
 		for (var x = 0; x < this.squaresX; x++) {
 			for (var y = 0; y < this.squaresY; y++) {
-				if (this.squareValue[x][y] > 0) {
-					var screenX = this.leftX + this.squareWidth * (x + 0.5) - 7.5;
-					var screenY = this.topY + this.squareHeight * (y + 0.5) + 11;
-					ctx.fillStyle = this.numColors[this.squareValue[x][y]];
-					ctx.fillText(this.squareValue[x][y], screenX, screenY);
+				if (this.covered[x][y]) { // draw cover
+					var screenX = this.leftX + this.squareWidth * x;
+					var screenY = this.topY + this.squareHeight * y;
+					ctx.strokeStyle = "#F0F0F0";
+					ctx.lineWidth = 3;
+					ctx.beginPath();
+					ctx.moveTo(screenX, screenY + 1.5);
+					ctx.lineTo(screenX + this.squareWidth, screenY + 1.5);
+					ctx.moveTo(screenX + 1.5, screenY);
+					ctx.lineTo(screenX + 1.5, screenY + this.squareHeight);
+					ctx.stroke();
+					ctx.strokeStyle = "#808080";
+					ctx.beginPath();
+					ctx.moveTo(screenX + this.squareWidth, screenY + this.squareHeight - 1.5);
+					ctx.lineTo(screenX, screenY + this.squareHeight - 1.5);
+					ctx.moveTo(screenX + this.squareWidth - 1.5, screenY + this.squareHeight);
+					ctx.lineTo(screenX + this.squareWidth - 1.5, screenY);
+					ctx.stroke();
 				}
-				else if (this.squareValue[x][y] == -1) { // change to draw bomb picture
-					var screenX = this.leftX + this.squareWidth * (x + 0.5) - 7.5;
-					var screenY = this.topY + this.squareHeight * (y + 0.5) + 11;
-					ctx.fillStyle = "#404040";
-					ctx.fillText("B", screenX, screenY);
+				else {
+					if (this.squareValue[x][y] > 0) { // draw number
+						var screenX = this.leftX + this.squareWidth * (x + 0.5) - 7;
+						var screenY = this.topY + this.squareHeight * (y + 0.5) + 8.5;
+						ctx.fillStyle = this.numColors[this.squareValue[x][y]];
+						ctx.fillText(this.squareValue[x][y], screenX, screenY);
+					}
+					else if (this.squareValue[x][y] == -1) { // change to draw bomb picture
+						var screenX = this.leftX + this.squareWidth * (x + 0.5) - 7;
+						var screenY = this.topY + this.squareHeight * (y + 0.5) + 8.5;
+						ctx.fillStyle = "#404040";
+						ctx.fillText("B", screenX, screenY);
+					}
 				}
 			}
 		}
 	}
+	ClickLoses(screenX, screenY) {
+		
+	}
 }
 
 myBoard = new Board(36, 16, 50, 50, 900, 400);
-myBoard.draw();
+
 
 
 
